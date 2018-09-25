759009591a761b08422da78cde07651a9311b27c
Jeremy Odell <jeremyodell@gmail.com>
2018-09-24 20:41:12 -0500

    node server
    Init checkin for node server

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+var server = require('./lib/server');
+const cors = require('cors');
+//var workers = require('./lib/workers');
+
+// Declare the app
+var app = {};
+
+// Init function
+app.init = function(){
+
+  // Start the server
+  server.init();
+
+  // Start the workers
+  //workers.init();
+
+};
+
+// Self executing
+app.init();
+
+
+// Export the app
+module.exports = app;
